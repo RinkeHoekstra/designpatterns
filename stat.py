a228c852b34f478e9c20f5b0fae59f7741893c71
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2015-05-20 16:32:23 +0200

    Logging based cache

--- stat.py
+++ stat.py
@@ -1,12 +1,36 @@
+#!/usr/bin/env python
+
 import csv
 import requests
 import argparse
-import string
-import random
+# import string
+# import random
+import logging
 
 from rdflib import Graph, URIRef
 from collections import defaultdict
 
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+handler = logging.FileHandler(filename='stat.log', mode='w', delay=True)
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(formatter)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(formatter)
+
+log.addHandler(handler)
+log.addHandler(console_handler)
+
+cache = logging.getLogger('cache')
+cache.setLevel(logging.INFO)
+cache_handler = logging.FileHandler(filename='cache.log', mode='a', delay=True)
+cache_handler.setLevel(logging.INFO)
+cache.addHandler(cache_handler)
 
 CACHE = "/Users/hoekstra/Dropbox/projects/designpatterns_stats/"
 PATTERN2URL = '/Users/hoekstra/Dropbox/projects/designpatterns_stats/sources.csv'
@@ -50,6 +74,28 @@ r2d = {}
 p2d = defaultdict(set)
 
 
+def get_stats(name):
+    filename = '{}.csv'.format(name)
+
+    try:
+        with open(filename) as cachefile:
+            cached_lines = cachefile.readlines()
+
+        cached_stats = {key: int(value) for [key, value] in [l.split(';') for l in cached_lines]}
+    except:
+        cached_stats = {}
+
+    stat_logger = logging.getLogger(name)
+    stat_logger.setLevel(logging.INFO)
+    stat_logger_handler = logging.FileHandler(filename=filename, mode='a', delay=True)
+    stat_logger_handler.setLevel(logging.INFO)
+    stat_logger.addHandler(stat_logger_handler)
+
+    return cached_stats, stat_logger
+
+
+
+
 def initialize_p2u():
     with open(PATTERN2URL, 'r') as f:
         reader = csv.reader(f, delimiter=';', quotechar='"')
@@ -79,21 +125,23 @@ def initialize_p2d():
 def generate_r2d():
     visited = set()
     for source, url in p2u.items():
-        print "Dataset:", url
+        log.info("Dataset: {}".format(url))
         filename = p2f[source]
 
         g = Graph()
         try:
             g.parse(filename, format='turtle')
         except Exception as e:
-            print "Could not parse document: ", filename
-            print e
+            log.error("Could not parse document: {}".format(filename))
+            log.error(e)
             continue
 
         subjects = [s for s in list(g.subjects()) if isinstance(s, URIRef)]
         predicates = [p for p in list(g.predicates())]
         objects = [o for o in list(g.objects()) if isinstance(o, URIRef)]
 
+        # Filter resources that are very (very) common to remove the
+        # possibilities for false positives.
         all_resources = [r for r in subjects + predicates + objects if
                          not('www.w3.org' in str(r)) and
                          not('dbpedia.org' in str(r)) and
@@ -102,7 +150,7 @@ def generate_r2d():
 
         for s in all_resources:
             if s not in visited:
-                print s
+                log.debug(s)
 
                 params = {'key': s}
 
@@ -122,7 +170,7 @@ def generate_r2d():
                 dataset_hashes = r2d[s]
                 p2d[url] = p2d[url].union(dataset_hashes)
 
-        print "P2D: ", len(p2d[url])
+        log.info("P2D: {}".format(len(p2d[url])))
 
     save_r2d()
     save_p2d()
@@ -173,18 +221,29 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    log.info('Initializing mappings from patterns to urls')
     initialize_p2u()
+
     if args.init:
+        log.info('Generating mapping from resources to documents')
         generate_r2d()
     else:
+        log.info('Initializing mappings from file')
         initialize_r2d()
         initialize_p2d()
 
-    print "Initialized..."
-
+    log.info("Initialized...")
     property_occurrences = {}
+
+    property_stats, property_statlogger = get_stats('property')
+    pattern_stats, pattern_statlogger = get_stats('pattern')
+
     for source, url in p2u.items():
-        print "Generalizing {}".format(url)
+        if url in pattern_stats:
+            log.info('Pattern {} was already visited'.format(url))
+            continue
+
+        log.info("Generalizing {}".format(url))
 
         filename = p2f[source]
         g = Graph()
@@ -192,28 +251,38 @@ if __name__ == '__main__':
         try:
             g.parse(filename, format='turtle')
         except Exception as e:
-            print "Could not parse document: ", filename
-            print e
+            log.error("Could not parse document: {}".format(filename))
+            log.error(e)
             continue
 
         lodl_documents = p2d[url]
 
         # Start counting from 0, oh, smart!
-        property_occurrences[url] = 0
+        pattern_stats[url] = 0
 
         results = g.query(DOMAIN_RANGE_QUERY)
 
         mapping = {}
 
-        print "Expecting occurrences in {} datasets.".format(len(lodl_documents))
-        properties = set([p for s, p, o in results])
+        log.info("Anticipating occurrences in {} datasets.".format(len(lodl_documents)))
+        properties = set([str(p) for s, p, o in results])
         for p in properties:
-            print "Property {}".format(p)
+            log.debug("Property {}".format(p))
+            if p in property_stats:
+                log.debug("Property {} was already visited".format(p))
+                # Add the known property stats count to the pattern stats
+                # And do not log it... for obvious reasons.
+                pattern_stats[url] += property_stats[p]
+                continue
+
+            # Start counting from 0, oh, smart!
+            property_stats[p] = 0
 
             for document_hash in lodl_documents:
+                log.debug("Checking for occurrences in dataset #{}".format(document_hash))
                 ldf_url = 'http://ldf.lodlaundromat.org/{}'.format(document_hash)
 
-                params = {'predicate': str(p)}
+                params = {'predicate': p}
 
                 result = requests.get(ldf_url, params=params,
                                       headers={'Accept': 'application/json'})
@@ -221,6 +290,15 @@ if __name__ == '__main__':
                 tpf = Graph()
                 tpf.parse(data=result.content, format='json-ld')
 
-                property_occurrences[url] += len(tpf)
+                property_stats[p] += len(tpf)
 
-    save_po(property_occurrences)
+            # Add the property to the cached list of visited properties
+            # (writing using a logger)
+            property_statlogger.info('{};{}'.format(p, property_stats[p]))
+            # Add the property stats to the statistics for the pattern in which
+            # it occurs
+            pattern_stats[url] += property_stats[p]
+
+        log.info("Found occurrences in {} datasets".format(pattern_stats[url]))
+        # Add the pattern stats to the cached list of visited patterns
+        pattern_statlogger.info('{};{}'.format(url, pattern_stats[url]))
